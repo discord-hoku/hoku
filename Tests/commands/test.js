92adcfb9c4812dfdea78664db075a854e9d61e3b
ndr3w221 <kalaz.bb@yahoo.com>
2018-09-08 21:33:21 +0300

    Commands now works

--- Tests/commands/test.js
+++ Tests/commands/test.js
@@ -1,1 +1,13 @@
-module.exports = {test: 'test'}
+module.exports = {
+    config: {
+        enabled: true, // is enabled?
+        name: 'ping' //, how to call it?
+    }, /*
+    help: {
+        displayName: 'test' // how to display the command in the help command
+    }*/
+    run: (message, args, client) => {
+        message.channel.send('Pong')
+    }
+
+}
--- src/Commands/startCommandListener.js
+++ src/Commands/startCommandListener.js
@@ -3,9 +3,17 @@ var GetCommands = require('./GetCommands')
 module.exports = async (Client) => {
     var commands = GetCommands(Client)
     console.log(commands)
-    Client.DiscordClient.on('message', (msg) => {
-        if (msg.author.bot) return;
-
-        console.log(msg.content)
+    Client.DiscordClient.on('message', (message) => {
+        if (message.author.bot) return;
+        
+        const args = message.content.slice(Client.opts.prefix.length).trim().split(/ +/g);
+        const command = args.shift().toLowerCase();
+        
+        commands.forEach(c => {
+            if (c.config.name === command) {
+                c.run(message, args, Client.DiscordClient)
+            }
+        })
+        
     })
 }
