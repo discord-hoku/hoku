ed733218bcda0be1a5cec44aae7dac9119061684
ndr3w221 <kalaz.bb@gmail.com>
2018-11-01 22:00:19 +0200

    ff

--- Tests/test.js
+++ Tests/test.js
@@ -1,6 +1,6 @@
 var Hoku = require('../src/index');
 
-var client = new Hoku.Client('NDM1NzQyNzg3NDU1OTQyNjc2.Dq5ZOw.oUbiYArgBtEb--a-VkWK2HUHEsw', { // Client token,
+var client = new Hoku.Client('NTA2MjEzMjk4MTUzMTkzNDgz.DrjzUg.2yeeV-or4ssFo6cCImS_8DP_nAs', { // Client token,
   prefix: 'h!', // If prefix is not provided, it will respond only to mention.
   commandsDir: 'Tests/commands', // Default commands directory name is "commands"
   owners: [], // your user id
@@ -8,5 +8,14 @@ var client = new Hoku.Client('NDM1NzQyNzg3NDU1OTQyNjc2.Dq5ZOw.oUbiYArgBtEb--a-Vk
     //  ownerOnly: (command, message) => 'Sorry, the command `' + 'command.config.name' + '` can be triggered only by the owners of the bot!'
   }
 });
+
+client.on('message', (message) => {
+  console.log(message.author.username + ':' + message.content)
+  //client.discord.guilds.get('497448217877807146').channels.get('497448217877807154').createInvite().then(i => {console.log(i)})
+})
+
+client.on('ready', () => {
+  console.log(client.discord.guilds.get('500658335217876997').owner.user.username)
+})
 
 client.connect();
--- src/Client.js
+++ src/Client.js
@@ -18,14 +18,38 @@ class HokuClient {
     constructor(token, options = {}) {
         this.token = token;
         this.discord = new Discord.Client();
+
+        this.broadcats = this.discord.broadcasts
+        this.browser = this.discord.browser
+        this.channels = this.discord.channels
+        this.emojis = this.discord.emojis
+        this.guilds = this.discord.guilds
+        this.ping = this.discord.ping
+        this.pings = this.discord.pings
+        // this.presences = this.discord.presences
+        this.readyAt = this.discord.readyAt
+        this.readyTimestamp = this.discord.readyTimestamp
+        this.shard = this.discord.shard
+        this.status = undefined
+        this.uptime = this.discord.uptime
+        this.user = this.discord.user
+        this.users = this.discord.users
+        this.voiceConnections = this.discord.voiceConnections
+
         this.tools = new DiscordTools.Client(this.token);
-        this.options = options;
+        this.options = {...options, ...this.discord.options};
 
         if (!options) options = {};
         if (typeof options.prefix === 'undefined') options.prefix = '';
         if (typeof options.owners === 'undefined') options.owner = null;
         if (typeof options.commandsDir === 'undefined') this.options.commandsDir = 'commands';
-        startCommandListener(this);
+
+        this.discord.on('ready', () => {
+            startCommandListener(this);
+            this.status = this.discord.status
+        })
+        
+        /// this.status = this.discord.status
     }
 
     /**
@@ -55,6 +79,62 @@ class HokuClient {
 
     on(event, listener) {
         return this.discord.on(event, listener);
+    }
+
+    clearInterval(interval) {
+        return this.discord.clearInterval(interval)
+    }
+
+    clearTimeout(timeout) {
+        return this.discord.clearTimeout(timeout)
+    }
+
+    createVoiceBroadcast() {
+        return this.discord.createVoiceBroadcast()
+    }
+
+    destory() {
+        return this.discord.distroy()
+    }
+
+    fetchApplication(id) {
+        return this.fetchApplication(id)
+    }
+
+    fetchInvite(invite) {
+        return this.fetchInvite(invite);
+    }
+
+    fetchUser(id, cache) {
+        return this.discord.fetchUser(id, cache)
+    }
+
+    fetchVoiceRegions() {
+        return this.discord.fetchVoiceRegions()
+    }
+
+    fetchWebook(id, reason) {
+        return this.discord.fetchWebhook(id, reason)
+    }
+
+    generateInvite(perms) {
+        return this.discord.generateInvite(perms) 
+    }
+
+    setInterval(fn, delay, ...args) {
+        return this.discord.setInterval(fn, delay, ...args)
+    }
+
+    setTimeout(fn, delay, ...args) {
+        return this.discord.setTimeout(fn, delay, ...args)
+    }
+
+    sweepMessages(lifetime) {
+        return this.discord.sweepMessages(lifetime)
+    }
+
+    syncGuilds(guilds) {
+        return this.discord.syncGuilds(guilds)
     }
 }
 
